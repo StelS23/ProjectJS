822c3e49cc5f105d42d3b0747be47aaba0bcce26
StelS23 <80637124+StelS23@users.noreply.github.com>
2023-06-01 20:07:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,126 @@
+// let name = 'Ivan';
+// alert(`Hello, ${name}!`);
+
+
+
+// let isGreated = 4 > 1;
+// alert(isGreated);
+
+
+
+// let age = prompt('Skolko tebe let', 100);
+// alert (`Tebe ${age} let`);
+
+
+
+// let isBoss = confirm('tu tyt boss');
+// alert(isBoss);
+
+
+
+// let yourName = prompt('Kak tebya zovut', );
+// alert (`Tebya zovut ${yourName}`);
+
+
+
+// alert('6'/'2');
+
+
+
+// let str = '123';
+// alert(typeof str);
+
+// let num = Number(str);
+
+// alert(typeof num);
+
+
+
+// alert(Number('123'));
+// alert(Number('123z'));
+// alert(Number('true'));
+// alert(Number('false'));
+
+
+
+// let x=1, y=3;
+// alert(x-y);
+
+
+
+// alert(2**8);
+
+
+
+// let s = 'my'+'stroke';
+// alert(s);
+
+
+
+// alert('1'+2);
+// alert(2+'1')
+
+
+
+
+// alert(2+2+'1');
+
+
+
+// alert(6-'2');
+// alert('6'/'2');
+
+
+
+// let apples='2', oranges='3';
+// alert(apples+oranges);
+
+
+
+// let apples='2', oranges='3';
+// alert(+apples + +oranges);
+
+
+
+// let a=1;
+// let b=2;
+// let c=3-(a=b+1);
+// alert(a);
+// alert(c);
+
+
+
+// let counter=2;
+// counter++;
+// alert(counter);
+
+
+
+// let counter=2;
+// counter--;
+// alert(counter);
+
+
+
+// Побитовые операторы
+// AND(и)&
+// OR(или)|
+// XOR(побитовое исключающее или)^
+// NOT(не)~
+// LEFT SHIFT(левый сдвиг)<<
+// RIGHT SHIFT(правый сдвиг)>>
+
+
+
+// let a=prompt('pervoe', '1');
+// let b=prompt('vtoroe', '2');
+// alert(+a + +b);
+
+
+
+// alert(2>1);
+// alert(2==1);
+// alert(2!=1);
+
+
+
